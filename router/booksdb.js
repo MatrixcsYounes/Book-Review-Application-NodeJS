7a2774e2f693eb38196cb2ee171cdcfc55c93fb3
MatrixcsYounes <younesho9@outlook.com>
2023-01-27 02:17:53 +0100

    Book review App Commit

---
+++ router/booksdb.js
@@ -0,0 +1,9 @@
+let books = {
+
+      1: {"author": "J.K.Rowling","title": "Harry Potter", "reviews": {} },
+      2: {"author": "Paulo Coelho","title": "The Alchemist", "reviews": {} },
+      3: {"author": "F. Scott Fitzgerald","title": "The Great Gatsby", "reviews": {} },
+      4: {"author": "Charlotte Brontë","title": "Jane Eyre", "reviews": {} }
+}
+
+module.exports=books;
